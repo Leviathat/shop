9ef39cf2c30f1675eed64dfbc46387b4ea0f31d1
Murager <jmmukanov@gmail.com>
2023-05-01 19:41:32 +0600

    Add models Admin panel

---
+++ backend/apps/product/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Product, Category
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
