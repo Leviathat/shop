7b0d2cbaf5124a8fcacff50a2399a390d5ef3796
Murager <jmmukanov@gmail.com>
2023-05-01 19:45:11 +0600

    Add images panel

--- backend/apps/product/admin.py
+++ backend/apps/product/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, ProductImage
 
 class ProductAdmin(admin.ModelAdmin):
     pass
@@ -7,5 +7,10 @@ class ProductAdmin(admin.ModelAdmin):
 class CategoryAdmin(admin.ModelAdmin):
     pass
 
+
+class ProductImageAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(ProductImage, ProductImageAdmin)
