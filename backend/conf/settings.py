00d179ac335f15ebec915e3a94af8966156a9ebd
Murager <jmmukanov@gmail.com>
2023-05-12 14:07:20 +0600

    Bugfix nginx media

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -145,7 +145,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/images/'
-MEDIA_ROOT = '/var/www/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
