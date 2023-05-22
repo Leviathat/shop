416adee0a6fc80c29e2c8b047e27f0cf49ac44a3
Murager <jmmukanov@gmail.com>
2023-05-22 21:29:34 +0600

    New Order status on save

--- backend/apps/order/models.py
+++ backend/apps/order/models.py
@@ -1,6 +1,8 @@
+from typing import Iterable, Optional
 from django.db import models
 from apps.product.models import Product
 from apps.authentication.models import User
+from django.dispatch import receiver
 
 
 class Order(models.Model):
@@ -11,13 +13,14 @@ class Order(models.Model):
         (3, 'В пути'),
         (4, 'Доставлен'),
         (5, 'Отменен'),
+        (6, 'Товар не в наличии'),
     ]
+    
     status = models.IntegerField(choices=STATUS_CHOICES, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     products = models.ManyToManyField(Product, through='OrderProduct')
     
-
     @property
     def total_amount(self):
         order_products = OrderProduct.objects.filter(order=self)
@@ -41,3 +44,10 @@ class OrderProduct(models.Model):
 
     class Meta:
         db_table = 'order_product'
+
+@receiver(models.signals.post_save, sender=OrderProduct)
+def update_order_status(sender, instance, **kwargs):
+    order = instance.order
+    if order.products.filter(in_stock=False).exists():
+        order.status = 6
+        order.save(update_fields=['status'])
--- frontend/src/api/api.config.js
+++ frontend/src/api/api.config.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const API_URL = '/api/';
+const API_URL = 'localhost:8000/api/';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
